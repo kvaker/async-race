bbc323ebd97eaf7c792aa87a1f568fd7d08a7e70
v-kuntsevich <kwakin@gmail.com>
2023-01-22 23:46:47 +0300

    feat: add popup and car image

---
+++ client/src/components/popup/winner-popup.ts
@@ -0,0 +1,19 @@
+import { ICarData } from '../../interfaces';
+import { Component } from '../../utils/component';
+import './winner-popup.scss';
+
+export class WinnerPopup extends Component {
+    constructor(parentNode: HTMLElement, carData: ICarData) {
+        super(parentNode, 'div', ['popup-winner']);
+
+        const title = new Component(this.element, 'h2', ['popup-winner-title']);
+        title.element.style.color = `${carData.color}`;
+        title.element.innerHTML = `
+      Winner 
+      <span>${carData.name}</span> 
+      in 
+      <span>${carData.speed}</span>
+      sec...
+    `;
+    }
+}
---
+++ client/src/components/shared/generate-rgb.ts
@@ -0,0 +1,9 @@
+export const randomRGBColor = (min = 0, max = 255) => {
+    const random = () => min + Math.floor(Math.random() * (max - min + 1));
+
+    const r = random();
+    const g = random();
+    const b = random();
+
+    return `rgb(${r},${g},${b})`;
+};
