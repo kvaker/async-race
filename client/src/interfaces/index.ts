92433329c3bad6a75cd5a0d614419463514dce9d
v-kuntsevich <kwakin@gmail.com>
2023-01-23 12:03:29 +0300

    feat: add api, configuration, interfaces

---
+++ client/src/interfaces/index.ts
@@ -0,0 +1,47 @@
+export interface IRoute {
+    name: string;
+    component: () => void;
+}
+
+export interface ICar {
+    id: number;
+    name: string;
+    color: string;
+}
+
+export interface ICreateCar {
+    name: string;
+    color: string;
+}
+
+export interface ICarEngine {
+    velocity: number;
+    distance: number;
+}
+
+export interface ICarDriveMode {
+    success: boolean;
+}
+
+export interface IUpdateCar {
+    id?: number;
+    name: string;
+    color: string;
+}
+
+export interface IWinner {
+    id: number;
+    time: number;
+    wins: number;
+}
+
+export interface ICarData {
+    id: number;
+    name: string;
+    color: string;
+    speed: number;
+    wins: number;
+    time?: number;
+}
+
+export interface IWinnerCar extends ICar, IWinner { }
